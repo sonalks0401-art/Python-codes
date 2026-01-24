fe708a1afe4a07c32c306d1d51bb7ac2a23fef69
Sonal Singh <sonalks0401@gmail.com>
2026-01-24 19:54:28 +0530

    1.0 codes

---
+++ 10codes.py
@@ -0,0 +1,58 @@
+print("Hello! I will introduce myself.")
+print("Name :Sonal Singh")
+print("Age :19")
+print("Course :B.Tech - CSE")
+print("Language: Python")
+
+
+
+
+print("---task 3: Circumference of a circle ---")
+radius = float(input("Enter radius of thr circle"))
+circumference_circle = 2*radius*3.14
+print(f"The circumference is:{circumference_circle:.2f}")
+
+
+print("---task 4: area of a circle---")
+radius = float(input("Enter the radius of the circle"))
+area_circle = radius*radius*3.14
+print(f"The area is:{area_circle:.2f}")
+
+print("---task:4 Area of the triangle---")
+
+
+base = float(input("Enter base: "))
+height = float(input("Enter height: "))
+area = 0.5* base* height
+print("Area of triangle: ",area)
+
+p=float(input("enter principal:"))
+r=float(input("enter rate of intetrest:"))
+t=float(input("enter time(in years):"))
+si=(p*r*t)/100
+print("simple intetrest:",si)
+
+celsius=float(input("enter temperature in celcius:"))
+fahrenheit=(celsius*9/5)+32
+print("temperature in fahrenheit:",fahrenheit)
+
+
+a=float(input("enter first number :"))
+b=float(input("enter second number:"))
+c=float(input("enter third number:"))
+average=(a+b+c)/3
+print("average:",average)
+
+
+length=float(input("enter length:"))
+breadth=float(input("enter breadth:"))
+area=length*breadth
+print("area of rectangle:",area)
+
+age_years=int(input("enter your age in years:"))
+age_days=age_years*365
+print("your age in days:",age_days)
+
+#sonal
+
+
